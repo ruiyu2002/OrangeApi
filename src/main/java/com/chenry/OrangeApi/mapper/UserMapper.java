4e7b23c494f307c5cb780a6444405e14137e93a7
chenry <18571509328@163.com>
2024-10-19 15:20:42 +0800

    OrangeApi项目骨架已搭建

---
+++ src/main/java/com/chenry/OrangeApi/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.chenry.OrangeApi.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.chenry.OrangeApi.model.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 用户数据库操作
+ *
+ */
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
